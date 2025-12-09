d4142bb97140340eb5f8baaaf1a2cf63bfa7b1b3
Sanat1427 <144201893+Sanat1427@users.noreply.github.com>
2025-12-10 03:16:32 +0530

    Time: 555 ms (76.98%), Space: 367.9 MB (67.63%) - LeetHub

---
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -0,0 +1,30 @@
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int specialTriplets(vector<int>& nums) {
+        const int MOD = 1e9 + 7;
+        int n = nums.size();
+        if (n < 3) return 0;
+
+        long long cnt = 0;
+        unordered_map<long long, int> leftFreq, rightFreq;
+
+        for (int i = 0; i < n; ++i) {
+            rightFreq[nums[i]]++;
+        }
+
+        for (int j = 0; j < n; ++j) {
+            rightFreq[nums[j]]--;
+            long long target = 2LL * nums[j];
+            long long leftCount = leftFreq.count(target) ? leftFreq[target] : 0;
+            long long rightCount = rightFreq.count(target) ? rightFreq[target] : 0;
+            cnt = (cnt + (leftCount * rightCount) % MOD) % MOD;
+            leftFreq[nums[j]]++;
+        }
+
+        return (int)cnt;
+    }
+};
