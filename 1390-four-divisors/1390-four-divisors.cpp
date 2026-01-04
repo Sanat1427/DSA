2df99479691195b86173bf065e8ab716a897a582
Sanat1427 <144201893+Sanat1427@users.noreply.github.com>
2026-01-05 00:04:28 +0530

    Time: 19 ms (63.3%), Space: 16.3 MB (32.76%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int ans = 0;
+
+        for (int x : nums) {
+            int cnt = 0;
+            int sum = 0;
+
+            for (int d = 1; d * d <= x; d++) {
+                if (x % d == 0) {
+                    cnt++;
+                    sum += d;
+
+                    if (d != x / d) {
+                        cnt++;
+                        sum += x / d;
+                    }
+                }
+                if (cnt > 4) break;
+            }
+
+            if (cnt == 4)
+                ans += sum;
+        }
+
+        return ans;
+    }
+};
