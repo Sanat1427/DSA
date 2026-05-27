c0b4d8e59c1e75f401886466d4cc00c681680cdf
Sanat <144201893+Sanat1427@users.noreply.github.com>
2026-05-28 02:39:06 +0530

    Time: 3 ms (78.41%), Space: 11.7 MB (14.92%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,32 +2,36 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         int n = s.size();
-        int mini = INT_MAX;
-        int l=0;
-        int r=0;
-        int sindex=-1;
+        int mini  = INT_MAX;
+        int l =0;
+        int r =0;
+        int si =-1;
         int hash[256]={0};
-        for(char c :t)
-        hash[c]++;
         int cnt =0;
+        for (char c :t){
+            hash[c]++;
+        }
         while(r<n){
             if(hash[s[r]]>0){
                 cnt++;
             }
             hash[s[r]]--;
-            while(cnt==t.size()){
-                if(r-l+1<mini){
-                    mini=r-l+1;
-                    sindex=l;
-                }
+           while (cnt==t.size()){
+              if(mini>r-l+1){
+                mini =  r-l+1;
+                si = l ; 
+              }
                 hash[s[l]]++;
-                if(hash[s[l]]>0){
+            
+              
+            if(hash[s[l]] > 0) {
                     cnt--;
                 }
-                l++;
-            }
+            l++;
+           }
             r++;
+
         }
-        return (sindex==-1)?"":s.substr(sindex,mini);
+        return (si==-1)?"":s.substr(si,mini);
     }
 };
