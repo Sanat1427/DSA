85fc398f9e7f4efdc61ca3f684f996b785dbeb44
Sanat <144201893+Sanat1427@users.noreply.github.com>
2026-05-28 01:55:03 +0530

    Time: 88 ms (39.89%), Space: 56.3 MB (18.56%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    int distinct(vector<int>& nums, int k){
-        int n = nums.size();
-        int l = 0, r = 0;
-        int ans = 0;
-        unordered_map<int,int> mpp;
-
-        while(r < n){
-            if(mpp[nums[r]]++ == 0){
-                k--;
-            }
-
-            while(k < 0){
-                mpp[nums[l]]--;
-                if(mpp[nums[l]] == 0){
-                    k++;
-                }
-                l++;
-            }
-
-            ans += r - l + 1;
-            r++;
+int func(vector<int>&nums, int k){
+    int n = nums.size();
+    int l =0;
+    int cnt =0;
+    unordered_map<int,int>mpp;
+    for(int right =0; right<n;right++){
+        if(mpp[nums[right]]==0){
+            //means we got the distinct element
+             k--;
         }
-        return ans;
+             mpp[nums[right]]++;
+        
+        while(k<0){
+            mpp[nums[l]]--;
+            if(mpp[nums[l]]==0){
+                mpp.erase(nums[l]);
+               k++;
+            }
+            l++;
+            
+        }
+        cnt+=right-l+1;
+        
     }
+    return cnt;
 
+}
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return distinct(nums, k) - distinct(nums, k - 1);
+        int n = nums.size();
+        return func(nums,k)-func(nums,k-1);
     }
 };
