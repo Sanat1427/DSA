552b1213406388c7cad261dfbfe24155ce978e80
Sanat <144201893+Sanat1427@users.noreply.github.com>
2026-05-30 01:40:10 +0530

    Time: 0 ms (100%), Space: 8.7 MB (17.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
+    int func(vector<int>& dp, int n) {
+        if (n <= 1)
+            return 1;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        return dp[n] = func(dp, n - 1) + func(dp, n - 2);
+    }
+
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        dp[0]=1;
-        dp[1]=1;
-        for(int i =2;i<n+1;i++){
-            dp[i]=dp[i-2]+dp[i-1];
-        }
-        return dp[n];
+        vector<int> dp(n + 1, -1);
+        return func(dp, n);
     }
 };
