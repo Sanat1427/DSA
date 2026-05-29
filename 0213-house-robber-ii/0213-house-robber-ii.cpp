9f3795ea3650d07878c6014b745455e219ca0bdf
Sanat <144201893+Sanat1427@users.noreply.github.com>
2026-05-30 02:37:54 +0530

    Time: 0 ms (100%), Space: 11 MB (7.76%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
-int func(int ind, vector<int>&arr,vector<int>&dp){
-    if(ind==0)
-    return arr[ind];
-    if(ind<0)
-    return 0;
-    if(dp[ind]!=-1)
-    return dp[ind];
-    int pick = arr[ind]+func(ind-2,arr,dp);
-    int nonpick= 0+func(ind-1,arr,dp);
-    return dp[ind]=max(pick,nonpick);
+    int func(vector<int>& arr) {
+        int n = arr.size();
 
-}
+        vector<int> dp(n, -1);
+        dp[0] = arr[0];
+
+        for(int i = 1; i < n; i++) {
+            int p = arr[i];
+            if(i > 1)
+                p += dp[i - 2];
+
+            int np = dp[i - 1];
+
+            dp[i] = max(p, np);
+        }
+
+        return dp[n - 1];
+    }
+
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>arr1;
-        vector<int>arr2;
-        if(n==1)
-        return nums[0];
-        for(int i =0;i<n;i++){
-            if(i!=n-1)
-            arr1.push_back(nums[i]);
-            if(i!=0)
-            arr2.push_back(nums[i]);
+
+        if(n == 1) return nums[0];
+
+        vector<int> arr1, arr2;
+
+        for(int i = 0; i < n; i++) {
+            if(i != 0) arr1.push_back(nums[i]);
+            if(i != n - 1) arr2.push_back(nums[i]);
         }
-        vector<int>dp(n,-1);
-        vector<int>dp1(n,-1);
-        int ans1=func(arr1.size()-1,arr1,dp);
-        int ans2=func(arr2.size()-1,arr2,dp1);
-        return max(ans1,ans2);
+
+        int ans1 = func(arr1);
+        int ans2 = func(arr2);
+
+        return max(ans1, ans2);
     }
 };
