2613635ad74d5c829a68e4525714d68ea9f4d885
Sanat <144201893+Sanat1427@users.noreply.github.com>
2026-06-02 15:01:03 +0530

    Time: 156 ms (29.38%), Space: 17.4 MB (58.15%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
-bool func(int ind,vector<int>& nums,  vector<vector<int>>&dp,int k){
-  if(k==0)
-  return true;
-  if(ind==0)
-  return nums[0]==k;
-  if(dp[ind][k]!=-1)
-  return dp[ind][k];
-  bool notaken = func(ind-1,nums,dp,k);
-  bool taken = false;
-  if(nums[ind]<=k)
-  taken=func(ind-1,nums,dp,k-nums[ind]);
-  return dp[ind][k]=notaken||taken;
-}
-    bool canPartition(vector<int>& nums) {
-        int n= nums.size();
-        int totalsum =0;
-        for(int i =0;i<n;i++){
-            totalsum+=nums[i];
+ bool func(vector<int>&arr, vector<vector<bool>>&dp,int ind , long long target){ 
+    int n = arr.size();
+   for(int i =0;i<n;i++){
+    dp[i][0]= true; // target ==0
+   }
+   if(arr[0]<=target){
+    dp[0][arr[0]]= true;
+   }
+   for(int ind =1;ind<n;ind++){
+    for(int i=1;i<=target;i++){
+        bool nott = dp[ind-1][i];
+        bool taken = false;
+        if(arr[ind]<=i){
+            taken = dp[ind-1][i-arr[ind]];
         }
-        if(totalsum%2==1)
+        dp[ind][i]= nott || taken;
+    }
+   }
+   return dp[n-1][target];
+ }
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        long long total =0;
+        for(int i =0;i<n;i++){
+            total+=nums[i];
+        }
+        if(total%2!=0)
         return false;
         else{
-            int k = totalsum/2;
-            vector<vector<int>>dp(n,vector<int>(k+1,-1));
-            return func(n-1,nums,dp,k);
-
+            long long k = total/2;
+            vector<vector<bool>>dp(n,vector<bool>(k+1,false));
+            return func(nums,dp,n-1,k);
         }
     }
 };
