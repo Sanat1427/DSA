c0f59f3df19a2442f30cc75f152afc523202f7e5
Sanat <144201893+Sanat1427@users.noreply.github.com>
2026-06-05 00:38:50 +0530

    Time: 51 ms (22.99%), Space: 49.5 MB (24.59%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-int func(int ind,vector<int>&coins,vector<vector<int>>&dp,int amount){
-    //base case
-    if(ind==0){
-        if(amount%coins[ind]==0)
-        return amount/coins[ind];
+    int func(int ind, vector<int>&coins,vector<vector<int>>&dp, int target){
+       if(ind==0){
+        if(target%coins[0]==0)
+        return target/coins[0];
+        else
         return 1e9;
+       }
+       if(dp[ind][target]!=-1)
+       return dp[ind][target];
+       int nottaken = func(ind-1,coins,dp,target);
+       int taken =1e9;
+       if(coins[ind]<=target){
+        taken= 1+func(ind,coins,dp,target-coins[ind]);
+       }
+        return dp[ind][target]= min(taken,nottaken);
     }
-    if(dp[ind][amount]!=-1)
-    return dp[ind][amount];
-    int nottaken= func(ind-1,coins,dp,amount);
-    int taken=1e9;
-    if(coins[ind]<=amount){
-        taken=1+func(ind,coins,dp,amount-coins[ind]);
-    }
-
-    return dp[ind][amount]=min(taken,nottaken);
-
-}
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-     vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-     int ans = func(n-1,coins,dp,amount);
-     if(ans>=1e9)
-     return -1;
-     return ans;
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int ans = func(n-1,coins,dp,amount);
+          return (ans >= 1e9) ? -1 : ans;
     }
 };
