34a1e4adc2598b52dab65ce2fdf95c1c1c32063c
Sanat <144201893+Sanat1427@users.noreply.github.com>
2026-06-13 14:44:34 +0530

    Time: 73 ms (5.4%), Space: 84.2 MB (5.04%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-int edit(string & s1, string & s2, vector<vector<int>>&dp, int i , int j ){
-    if(i<0)
-    return j+1;
-    if(j<0)
-    return i+1;
-    if(dp[i][j]!=-1)
-    return dp[i][j];
-    if(s1[i]==s2[j])
-    return dp[i][j]=0+edit(s1,s2,dp,i-1,j-1);
-    else
-    return dp[i][j]=1+min(edit(s1,s2,dp,i-1,j-1),min(edit(s1,s2,dp,i-1,j),edit(s1,s2,dp,i,j-1)));
-
-}
+    int func(string word1, string word2, int i, int j, vector<vector<int>>&dp){
+        if(i<0)
+        return j+1;
+        if(j<0)
+        return i+1;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        if(word1[i]==word2[j]){
+            return dp[i][j]=func(word1,word2,i-1,j-1,dp);
+            //not add 1 as func is already matching
+        }
+        else{
+            return dp[i][j]=1+min(func(word1,word2,i-1,j-1,dp),min(func(word1,word2,i-1,j,dp),func(word1,word2,i,j-1,dp)));
+        }
+    }
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        return edit(word1,word2,dp,n-1,m-1);
+        return func(word1,word2,n-1,m-1,dp);
     }
 };
