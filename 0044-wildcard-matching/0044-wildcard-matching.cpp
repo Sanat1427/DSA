80bb09916771b7e51207985a0efb4667910b0da8
Sanat <144201893+Sanat1427@users.noreply.github.com>
2026-06-13 18:17:08 +0530

    Time: 40 ms (69.24%), Space: 14 MB (73.88%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -10,12 +10,14 @@ public:
         }
         for(int i =1;i<=m;i++){
             for(int j =1;j<=n;j++){
-                if(s[j-1]==p[i-1]||p[i-1]=='?')
+                if((p[i-1]==s[j-1])||p[i-1]=='?')
                 dp[i][j]=dp[i-1][j-1];
-                else if (p[i-1]=='*')
-                dp[i][j]=dp[i-1][j]||dp[i][j-1];
-                else
-                dp[i][j]=false;
+                else if(p[i-1]=='*'){
+                    dp[i][j]=dp[i-1][j]||dp[i][j-1];
+                }
+                else{
+                    dp[i][j]=false;
+                }
             }
         }
         return dp[m][n];
