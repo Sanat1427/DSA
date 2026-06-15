cbe31a92d0661f83e3d415a7c4b52ae93fc43584
Sanat <144201893+Sanat1427@users.noreply.github.com>
2026-06-15 13:10:38 +0530

    Time: 0 ms (100%), Space: 16.2 MB (49.75%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -6,6 +6,7 @@ public:
         for(int i= n-1;i>=0;i--){
             for(int h =0;h<=1;h++){
                 //holding the stock
+                // i+2 as if we sell today so we need cooldown for next day that's why i+2
                 if(h==1){
             dp[i][1]=max(prices[i]+dp[i+2][0],dp[i+1][1]);
                 }
