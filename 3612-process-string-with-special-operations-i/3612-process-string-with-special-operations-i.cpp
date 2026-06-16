f47735f48c6237850a8bc2ee89c50082eb24bec9
Sanat <144201893+Sanat1427@users.noreply.github.com>
2026-06-17 03:32:14 +0530

    Time: 4 ms (63.88%), Space: 39.8 MB (42.97%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result;
+        for (char ch : s) {
+            if (ch != '*' && ch != '#' && ch != '%') {
+                result.push_back(ch);
+            } else if (ch == '#') {
+                result += result;
+            } else if (ch == '*' && !result.empty()) {
+                result.pop_back();
+            } else if (ch == '%') {
+                reverse(result.begin(), result.end());
+            }
+        }
+        return result;
+    }
+};
