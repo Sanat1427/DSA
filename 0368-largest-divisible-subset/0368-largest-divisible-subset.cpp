8b12d14a928191db0327c46a986a015e6c661c46
Sanat <144201893+Sanat1427@users.noreply.github.com>
2026-06-19 01:33:43 +0530

    Time: 7 ms (93.4%), Space: 12.7 MB (19.31%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>ans;
-        vector<int>dp(n,1);
-        vector<int>parent(n);
-        int maxi=0;
-        int last=0;
-        sort(nums.begin(),nums.end());
-        for(int i =0;i<n;i++){
-            parent[i]=i;
-            for(int j =0;j<i;j++){
-                if(nums[i]%nums[j]==0 && dp[i]<dp[j]+1){
-                    dp[i]=dp[j]+1;
-                    parent[i]=j;
+           int n = nums.size();
+           sort(nums.begin(),nums.end());
+            vector<int>ans;
+            vector<int>dp(n+1,0);
+            vector<int>parent(n);
+            int maxi=0;
+            int last =0;
+            for(int i =0;i<n;i++){
+                parent[i]=i;
+                for(int prev=0;prev<i;prev++){
+                    if(nums[i]%nums[prev]==0&& dp[i]<dp[prev]+1){
+                        dp[i]=dp[prev]+1;
+                        parent[i]=prev;
+                        
+                        
+                    }
+                    
+
                 }
                 if(dp[i]>maxi){
-                    maxi=dp[i];
-                    last=i;
-                }
+                        last =i;
+                        maxi=dp[i];
+                    
+                    }
             }
-            
-        }
-        int i = last;
+            int i = last;
             while(parent[i]!=i){
                 ans.push_back(nums[i]);
-                i=parent[i];
+                i= parent[i];
             }
             ans.push_back(nums[i]);
+            reverse(ans.begin(),ans.end()); 
             return ans;
+                   
     }
 };
