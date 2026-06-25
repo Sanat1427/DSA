c0eea4b746c8222729b1b52b5df9b39cf2a656ab
Sanat <144201893+Sanat1427@users.noreply.github.com>
2026-06-25 14:45:09 +0530

    Time: 27 ms (27.95%), Space: 38.6 MB (40.43%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,29 +1,24 @@
 class Solution {
-    vector<int>delrow={1,0,-1,0};
-    vector<int>delcol={0,-1,0,1};
-
-    bool isvalid(int&i,int&j,int&n,int&m){
-        if(i<0||i>=n)return false;
-        if(j<0||j>=m)return false;
+public:
+vector<int>delRow={-1,0,1,0};
+    vector<int>delCol ={0,1,0,-1};
+    bool isvalid(int & i , int & j , int & n, int & m){
+        if(i<0 || i>=n) return false;
+        if(j<0 || j>=m ) return false;
         return true;
     }
-
-public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
+        vector<vector<int>>ans=mat;
         vector<vector<int>>vis(n,vector<int>(m,0));
-        vector<vector<int>>dist(n,vector<int>(m,0));
-        queue<pair<pair<int,int>,int>>q;
+         queue<pair<pair<int,int>, int >>q;
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
-              if(mat[i][j]==0){
-                q.push({{i,j},0});
-                vis[i][j]=1;
-              }
-              else{
-                vis[i][j]=0;
-              }
+                if(mat[i][j]==0){
+                    vis[i][j]=1;
+                    q.push({{i,j},0});
+                }
             }
         }
         while(!q.empty()){
@@ -31,19 +26,16 @@ public:
             q.pop();
             int row = it.first.first;
             int col = it.first.second;
-            int steps= it.second;
-            dist[row][col]=steps;
+            int step = it.second;
+            ans[row][col]=step;
             for(int i =0;i<4;i++){
-                int nrow=row+delrow[i];
-                int ncol=col+delcol[i];
-                if(isvalid(nrow,ncol,n,m)&& vis[nrow][ncol]==0){
-                    vis[nrow][ncol]=1;
-                    q.push({{nrow,ncol},steps+1});
-                }
-
+                int delrow = row+delRow[i];
+                int delcol = col+delCol[i];
+                if(isvalid(delrow,delcol,n,m)&&!vis[delrow][delcol]){
+                    vis[delrow][delcol]=1;
+                    q.push({{delrow,delcol},step+1});             }
             }
         }
-
-        return dist;
+        return ans;
     }
 };
