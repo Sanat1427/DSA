c8640db7fa3eef4dc67b697913b9914d81d395c3
Sanat <144201893+Sanat1427@users.noreply.github.com>
2026-06-26 16:50:59 +0530

    Time: 8 ms (34.26%), Space: 20.1 MB (24.57%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,43 +1,40 @@
 class Solution {
-    vector<int> toposort(int n, vector<vector<int>>& adj) {
-        vector<int> indegree(n, 0);
-        queue<int> q;
-
-        for (int i = 0; i < n; i++) {
-            for (auto it : adj[i]) {
-                indegree[it]++;
+public:
+     vector<int>toposort(int n , vector<vector<int>>&adj){
+        vector<int>ans;
+        vector<int>ind(n,0);
+        queue<int>q;
+        for(int i =0;i<n;i++){
+            for(auto it : adj[i]){
+                ind[it]++;
             }
         }
-
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0)
+        for(int i =0;i<n;i++){
+            if(ind[i]==0){
                 q.push(i);
+            }
         }
-
-        vector<int> ans;
-        while (!q.empty()) {
+        while(!q.empty()){
             int node = q.front();
             q.pop();
             ans.push_back(node);
-            for (auto it : adj[node]) {
-                indegree[it]--;
-                if (indegree[it] == 0)
-                    q.push(it);
+            for(auto it : adj[node]){
+                ind[it]--;
+                if(ind[it]==0)
+                q.push(it);
             }
         }
         return ans;
-    }
-
-public:
+     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        for (auto it : prerequisites) {
-            int u = it[0];
-            int v = it[1];
-            adj[v].push_back(u);
-        }
-
-        vector<int> topo = toposort(numCourses, adj);
-        return topo.size() == numCourses;
+         vector<int>topo;
+         vector<vector<int>>adj( numCourses);
+         for(auto it :prerequisites ){
+            adj[it[1]].push_back(it[0]);
+         }
+         topo= toposort(numCourses,adj);
+         if(topo.size()== numCourses)
+         return true;
+         return false;
     }
 };
