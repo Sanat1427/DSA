daed7d430af7365403ce35180dd276871bfc6e5d
Sanat <144201893+Sanat1427@users.noreply.github.com>
2026-06-29 15:56:56 +0530

    Time: 12 ms (38.76%), Space: 37.5 MB (33.28%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -1,38 +1,44 @@
 class Solution {
 public:
-#define P pair<long long , int>
+       int mod = 1e9+7;
     int countPaths(int n, vector<vector<int>>& roads) {
-        int mod = 1e9+7;
-      vector<vector<pair<int,int>>> adj(n);
+     
+        vector<vector<pair<int,int>>>adj(n);
+        vector<long long>mini(n,LLONG_MAX);
+        vector<int>ways(n,0);
         for(auto it : roads){
-            adj[it[0]].push_back({it[1],it[2]});
-            adj[it[1]].push_back({it[0],it[2]});  }
-            
-            vector<long long>mintime(n,LLONG_MAX);
-            vector<int>ways(n,0);
-            priority_queue<P,vector<P>,greater<P>>pq;
-            mintime[0]=0;
-            ways[0]=1;
-            pq.push({0,0});
-            while(!pq.empty()){
-                auto p = pq.top();
-                pq.pop();
-                long long time = p.first;
-                int node = p.second;
-                for(auto it : adj[node]){
-                    int adjnode = it.first;
-                    int traveltime = it.second;
-                    if(time+traveltime<mintime[adjnode]){
-                        mintime[adjnode]=time+traveltime;
-                        ways[adjnode]=ways[node];
-                        pq.push({mintime[adjnode],adjnode});
-                    }
-                    else if(mintime[adjnode]==time+traveltime){
-                        ways[adjnode]=(ways[adjnode]+ways[node])%mod;
-                    }
+            int u = it[0];
+            int v = it[1];
+            int t = it[2];
+            adj[u].push_back({v,t});
+            adj[v].push_back({u,t});
+        }
+         priority_queue<
+    pair<long long,int>,
+    vector<pair<long long,int>>,
+    greater<pair<long long,int>>
+> pq;
+         mini[0]=0;
+         ways[0]=1;
+         pq.push({0,0});
+         while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
+            long long t = it.first;
+            int node = it.second;
+            for(auto it : adj[node]){
+                int v = it.first;
+                int tt= it.second;
+                if(mini[v]>tt+t){
+                    mini[v]=tt+t;
+                    ways[v]=ways[node];
+                    pq.push({tt+t,v});
                 }
-
+                else if (mini[v]==tt+t){
+                    ways[v]=(ways[v]+ways[node])%mod;
+                }
             }
-            return ways[n-1]%mod;
+         }
+         return ways[n-1]%mod;
     }
 };
