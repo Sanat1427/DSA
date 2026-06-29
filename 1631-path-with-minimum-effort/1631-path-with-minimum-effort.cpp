4faea16cf74a520ae73d89a831f36bcf638d1b18
Sanat <144201893+Sanat1427@users.noreply.github.com>
2026-06-29 13:59:00 +0530

    Time: 45 ms (81.23%), Space: 24.5 MB (77.35%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,41 +1,49 @@
 class Solution {
-    #define P pair <int, pair<int,int>>
-    vector<int>delrow={-1,0,1,0};
-    vector<int>delcol={0,1,0,-1};
-    bool isvalid(int&row,int&col,int&n,int&m){
-        if(row<0||row>=n)return false;
-        if(col<0||col>=m)return false;
+public:
+#define P pair <int, pair<int,int>>
+
+  vector<int> delrow = {-1, 0, 1, 0};
+    vector<int> delcol = {0, -1, 0, 1};
+    
+    
+    bool isValid(int &row, int &col, 
+                 int &n, int &m) {
+                     
+   
+        if(row < 0 || row >= n) return false;
+        if(col < 0 || col >= m) return false;
+        
+     
         return true;
     }
-public:
+
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int n = heights.size();
-        int m = heights[0].size();
-        vector<vector<int>>maxdiff(n,vector<int>(m,1e9));
+         int n = heights.size();
+         int m = heights[0].size();
+         vector<vector<int>>maxdiff(n,vector<int>(m,1e9));
          priority_queue <P, vector<P>, greater<P>> pq;
          maxdiff[0][0]=0;
          pq.push({0,{0,0}});
          while(!pq.empty()){
-              auto p = pq.top();
-              int diff = p.first;
-              int row = p.second.first;
-              int col = p.second.second;
-              pq.pop();
-              if(row==n-1 && col==m-1)
-              return diff;
-
-              for(int i =0;i<4;i++){
-                int newrow =row+delrow[i];
-                int newcol = col+delcol[i];
-                if(isvalid(newrow,newcol,n,m)){
-                    int curdiff =  abs(heights[newrow][newcol]-heights[row][col]);
-                    if(max(curdiff,diff)<maxdiff[newrow][newcol]){
-                        maxdiff[newrow][newcol]=max(curdiff,diff);
-                        pq.push({max(curdiff,diff),{newrow,newcol}});
+            auto it = pq.top();
+            pq.pop();
+            int diff = it.first;
+            int row = it.second.first;
+            int col = it.second.second;
+            if(row==n-1 && col==m-1)
+            return diff;
+            for(int i =0;i<4;i++){
+                int nrow = row+delrow[i];
+                int ncol = col+delcol[i];
+                if(isValid(nrow,ncol,n,m)){
+                    int curdiff= abs(heights[nrow][ncol]-heights[row][col]);
+                    if(max(curdiff,diff)<maxdiff[nrow][ncol]){
+                        maxdiff[nrow][ncol]=max(curdiff,diff);
+                        pq.push({maxdiff[nrow][ncol],{nrow,ncol}});
                     }
                 }
-              }
+            }
          }
-         return -1;
+         return 0;
     }
 };
