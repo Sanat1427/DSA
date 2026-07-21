e1886d1def8fa2ce53db5efcdc13e70e73ea7736
Sanat <144201893+Sanat1427@users.noreply.github.com>
2026-07-22 03:38:35 +0530

    Time: 0 ms (100%), Space: 11.7 MB (26.59%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n = s.size();
-        int mini  = INT_MAX;
-        int l =0;
-        int r =0;
-        int si =-1;
-        int hash[256]={0};
-        int cnt =0;
-        for (char c :t){
-            hash[c]++;
-        }
+         int n = s.size();
+         int m = t.size();
+         int start =0;
+         int hash[256]={0};
+         for(int i =0;i<m;i++){
+             hash[t[i]]++;
+         }  
+         int l = 0;
+         int r =0;
+         int cnt =0;
+         int mini = INT_MAX;
         while(r<n){
             if(hash[s[r]]>0){
                 cnt++;
             }
             hash[s[r]]--;
-           while (cnt==t.size()){
-              if(mini>r-l+1){
-                mini =  r-l+1;
-                si = l ; 
-              }
+            while(cnt==t.size()){
+                    if (r - l + 1 < mini) {
+                    mini = r - l + 1;
+                    start = l;
+                }
                 hash[s[l]]++;
-            
-              
-            if(hash[s[l]] > 0) {
+                if(hash[s[l]]>0){
                     cnt--;
                 }
-            l++;
-           }
-            r++;
+                l++;
 
+            }
+            r++;
         }
-        return (si==-1)?"":s.substr(si,mini);
+        if(mini == INT_MAX)
+        return "";
+        return s.substr(start,mini);     
     }
 };
