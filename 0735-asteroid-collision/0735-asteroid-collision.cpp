85c922dbde71f40544fd6ab79635e997a8bd2980
Sanat <144201893+Sanat1427@users.noreply.github.com>
2026-07-27 23:49:29 +0530

    Time: 0 ms (100%), Space: 21.5 MB (90.06%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -4,21 +4,18 @@ public:
         int n = asteroids.size();
         vector<int>st;
         for(int i =0;i<n;i++){
-            if(asteroids[i]>0){
-                st.push_back(asteroids[i]);
-            }
+            if(asteroids[i]>0)
+            st.push_back(asteroids[i]);// only push psotive number
             else{
-            while(!st.empty() && st.back()>0 && st.back()<abs(asteroids[i])){
-                st.pop_back();
-
+            while(!st.empty()&& st.back()>0 && st.back()<abs(asteroids[i])){
+                st.pop_back();// ex 10 vs -20 , so pop_back(10)
+            
             }
-             if(!st.empty() && st.back()>0 && st.back()==abs(asteroids[i])){
-                st.pop_back();
-
-            }
-           else if(st.empty()|| st.back()<0){
-                st.push_back(asteroids[i]);
-            }
+            if(!st.empty()&& st.back()>0 && st.back()==abs(asteroids[i]))
+            st.pop_back(); // ex 10 vs -10
+            else if(st.empty() || st.back()<0)
+            st.push_back(asteroids[i]);
+            // ex- empty stack or -5 -10 , so then push negative number
             }
         }
         return st;
